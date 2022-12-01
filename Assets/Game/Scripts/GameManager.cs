e3cf899c99a25d8334f9df2d7bb54d2b0a91aacc
DaskalosJSM <116764636+DaskalosJSM@users.noreply.github.com>
2022-11-30 20:32:51 -0500

    Initial commit

---
+++ Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum GameState
+{
+    principalMenu,
+    Game,
+    gameOver,
+    Credits,
+    Controls
+}
+
+public class GameManager : MonoBehaviour
+{
+    // Inicializo el singleton en el primer script 
+    public static GameManager sharedInstance;
+
+    // Declaración del estado del juego
+    public GameState currentGameState = GameState.principalMenu;
+
+    public void Awake()
+    {
+        // que despierte y enfatizo con el siguiente fragmento
+        // Singleton
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Función encargado de iniciar la scena menú principal
+    public void PrincipalMenu()
+    {
+        SetGameState(GameState.principalMenu);
+    }
+    // Función encargado de iniciar la scena créditos
+    public void Credits()
+    {
+        SetGameState(GameState.Credits);
+    }
+    // Función encargado de iniciar la scena Game
+    public void Game()
+    {
+        SetGameState(GameState.Game);
+    }
+    // Función encargado de iniciar la scena controls
+    public void Controls()
+    {
+        SetGameState(GameState.Controls);
+    }
+    // Función encargado de iniciar la scena de final de juego
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    public void SetGameState(GameState newGameState)
+    {
+        this.currentGameState = newGameState;
+
+        if (newGameState == GameState.principalMenu)
+        {
+            //TODO: colocar la logica del menu
+            SceneManager.LoadScene("PrincipalMenu");
+        }
+        else if (newGameState == GameState.Game)
+        {
+            //TODO: colocar la logica del level 5
+            SceneManager.LoadScene("Game");
+        }
+        else if (newGameState == GameState.Credits)
+        {
+            //TODO: colocar la logica del level 5
+            SceneManager.LoadScene("Credits");
+        }
+        else if (newGameState == GameState.Controls)
+        {
+            //TODO: colocar la logica del level 5
+            SceneManager.LoadScene("Controls");
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            //TODO: colocar la logica del gameOver
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
